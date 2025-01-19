e3175299946d6434e00e46a0ff696637827edf65
kiranmayee57# <kiranmayee118@gmail.com>
2025-01-19 13:35:25 +0530

    Created PostgreSQL database and user for CRM

--- main.go
+++ main.go
@@ -4,10 +4,27 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
+
+var db *gorm.DB
+var err error
+
+func setupDatabase() {
+	dsn := "host=localhost user=crm_user password=password dbname=crm port=5432 sslmode=disable"
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database: ", err)
+	}
+	log.Println("Database connected successfully")
+}
 
 func main() {
 	app := fiber.New()
+
+	// Setup Database
+	setupDatabase()
 
 	// Home Route
 	app.Get("/", func(c *fiber.Ctx) error {
@@ -15,5 +32,5 @@ func main() {
 	})
 
 	// Start server
-	log.Fatal(app.Listen(":8081"))
+	log.Fatal(app.Listen(":8080"))
 }
